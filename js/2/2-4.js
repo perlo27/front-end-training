fcfae625c82af1000b79411cb993cd9f4d18da14
Mateusz Rataj <mati.rataj@gmail.com>
2018-06-11 10:56:19 +0200

    2-4 Variables names

---
+++ js/2/2-4.js
@@ -0,0 +1,14 @@
+// first task
+let admin;
+let name;
+name = "John";
+admin = name;
+console.log(admin);
+
+// second task
+let ourPlanet = "Earth";
+let currentVisitor = "Mati";
+
+// third task
+const BIRTHDAY =  '18.04.1982';
+const age = someCode(BIRTHDAY);
