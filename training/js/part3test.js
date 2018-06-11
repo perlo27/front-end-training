05d5c03bcc2870c7a3019195711d00472c4bb107
chermip <pavel.chermianinov@ihg.com>
2018-06-11 15:22:42 +0200

    Task3 - JS basics

---
+++ training/js/part3test.js
@@ -0,0 +1,16 @@
+
+
+describe("min", function() {
+
+  before(() => console.log("Testing started – before all tests"));
+  after(() => console.log("Testing finished – after all tests"));
+
+  it("2 less than 3", function() {
+    assert.equal(min(2, 3), 2);
+  });
+
+  it("4 less than 5", function() {
+    assert.equal(min(5, 4), 4);
+  });
+
+});
