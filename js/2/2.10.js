c2d50fd95b6004fa680571bcceb580cef4bf5cd9
Marek_Tabor <marektabor1@gmail.com>
2018-06-12 16:16:35 +0200

    2.10.7 js

--- js/2/2.10.js
+++ js/2/2.10.js
@@ -1,6 +1,68 @@
+'use strict'
+
 //2.10.1
 if ("0") { //any string except an empty one returns true
     console.log('Hello');
 }
 
 //2.10.2
+let answer;
+// answer = prompt('what is the official name of JS?', '');
+if(answer == 'ECMAScript') {
+    console.log('Right!')
+} else {
+    console.log("You don't know? ECMAScript")
+}
+
+//2.10.3
+let number;
+if(number > 0) {
+    console.log('1');
+} else if(number < 0) {
+    console.log('-1')
+} else {
+    console.log('0')
+}
+
+//2.10.4
+let userName = 'Admin';
+
+if (userName == 'Admin') {
+  let pass = 'TheMaster';
+  if (pass == 'TheMaster') {
+    console.log( 'Welcome!' );
+  } else if (pass == '' || pass == null) {
+    console.log( 'Canceled.' );
+  } else {
+    console.log( 'Wrong password' );
+  }
+} else if (userName == '' || userName == null) {
+  console.log( 'Canceled' );
+} else {
+  console.log( "I don't know you" );
+}
+
+//2.10.5
+// if (a + b < 4) {
+//     result = 'Below';
+// } else {
+//     result = 'Over';
+// }
+let a,b,result;
+a + b < 4 ? result = 'Below' : result = 'Over'
+
+//2.10.6
+let message,login = 'Director';
+// if (login == 'Employee') {
+//   message = 'Hello';ł
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+message = login == 'Employee' ? 'Hello' :
+login == 'Director' ? 'Greetings':
+login == '' ?  'No login' : 
+'';
---
+++ js/2/2.11.js
@@ -0,0 +1,21 @@
+console.log( null || 2 || undefined ); //2
+
+console.log(console.log(1) || 2 || console.log(3)); //1 2 
+
+console.log(1 && null && 2); //null
+
+console.log(console.log(1) && console.log(2)); //1 undefined
+
+console.log( null || 2 && 3 || 4); //3
+
+let age = 99;
+if(age >= 14 && age <= 90){}
+
+if(!(age >= 14 && age <= 90)){
+    console.log(age);
+}
+
+if(age <= 14 || age >= 90) {
+    console.log(age);
+}
+
