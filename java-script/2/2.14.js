367373589caac0045980c66c10359a19fadca334
Marcin <marcin_smola@epam.com>
2018-06-12 13:05:16 +0200

    task 2.14 - prime numbers

---
+++ java-script/2/2.14.js
@@ -0,0 +1,17 @@
+function showPrimes(n) {
+
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
+
+    console.log(i);  // a prime
+  }
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if ( n % i == 0) return false;
+  }
+  return true;
+}
+
+showPrimes(100);
--- java-script/2/2.4.js
+++ java-script/2/2.4.js
@@ -1,6 +1,11 @@
+//1
 let admin, name;
 name = "John";
 admin = name;
 
 console.log(name);
 console.log(admin);
+
+//2
+let ourPlanetName = "Earth";
+let currentVisitorName = "John";
