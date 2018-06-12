47178439b871da2be69568f3c9edeca4985f7b17
Marcin <marcin_smola@epam.com>
2018-06-12 15:56:12 +0200

    tasks 2.x

---
+++ java-script/2/2.11.js
@@ -0,0 +1,3 @@
+if (-12 || 0) console.log('first'); // pring
+if (-1 && 0) console.log('second');  //nothing
+if (null || -1 && 1) console.log('third'); //print
---
+++ java-script/2/2.15.js
@@ -0,0 +1,7 @@
+
+let xd = function welcome(name, age) {
+    (age < 18) ? console.log("Hello!" + name) : console.log("Greetings!" + name);
+}
+
+
+xd("Marcin", 17); // ok now
