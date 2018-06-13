e6216940a41b70ffef7231ccebd611f3f942470d
Marek_Tabor <marektabor1@gmail.com>
2018-06-13 09:19:50 +0200

    2.11 finished

--- js/2/2.11.js
+++ js/2/2.11.js
@@ -19,4 +19,6 @@ if(age <= 14 || age >= 90) {
     console.log(age);
 }
 
-    
+if (-1 || 0) console.log(-1 || 0 ); // -1
+if (-1 && 0) console.log(-1 && 0 ); // 0
+if (null || -1 && 1) console.log( null || -1 && 1 ); //1
