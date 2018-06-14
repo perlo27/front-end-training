e7c5dc7d7e3b06e9c0a64c7be8b7c145de9f155f
Marcin <marcin_smola@epam.com>
2018-06-14 14:15:50 +0200

    babel works

--- java-script/4/src/4/2.js
+++ java-script/4/src/4/2.js
@@ -1,16 +1,24 @@
+// export default class Calculator {
 
-let a,b;
-export class Calculator {
-    
-    read() {
-    this.a = +prompt("Enter first value: ");
-    this.b = +prompt("Enter second value: ");
-    };
-    sum() {
-        return this.a + this.b;
-    };
-    mul () {
-        return this.a * this.b;
+//     // read() {
+//     //     this.a = +prompt("Enter first value: ");
+//     //     this.b = +prompt("Enter second value: ");
+//     // }
+
+//     // sum() {
+//     //     return this.a + this.b;
+//     // }
+
+//     // mul() {
+//     //     return this.a * this.b;
+//     // }
+
+// }
+
+export function ask(isConfirmed, yes, no) {
+    if (isConfirmed) {
+      return yes();
+    } else {
+      return no();
     }
-}
-
+  }
--- java-script/4/test/xd/2-test.js
+++ java-script/4/test/xd/2-test.js
@@ -1,8 +1,49 @@
-import Calculator from '../../src/4/2.js';
-const chai = require('chai');
+import {ask} from '../../src/4/2.js';
+import {assert} from 'chai';
 
-describe("is Empty", function(){
-    it("return true when object has no properties",function(){
-       Calculator.sum();
+// describe("is Empty", function(){
+//     it("return true when object has no properties",function(){
+//        Calculator.xd();
+//     });
+// });
+
+describe("are you adult ?", function() {
+    it("if true return yes", function() {
+      let result = ask(
+        true,
+        () => {
+          return "yes";
+        },
+        () => {
+          return "no";
+        }
+      );
+      assert.equal(result, "yes");
     });
-});
+  
+    it("if false return no", function() {
+      let result = ask(
+        false,
+        () => {
+          return "yes";
+        },
+        () => {
+          return "no";
+        }
+      );
+      assert.equal(result, "no");
+    });
+  
+    it("if null return no", function() {
+      let result = ask(
+        null,
+        () => {
+          return "yes";
+        },
+        () => {
+          return "no";
+        }
+      );
+      assert.equal(result, "no");
+    });
+  });
