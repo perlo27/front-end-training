35a2a266822d25ed9da29ea070ef2e2bef4bbed8
Marcin <marcin_smola@epam.com>
2018-06-18 09:59:30 +0200

    calculator works

--- java-script/4/src/5/4.js
+++ java-script/4/src/5/4.js
@@ -1,12 +1,22 @@
 'use strict';
 
-class Calculator {
-    constructor() { }
-    sum() {
-        console.log(this.expression);
+function Calculator() {
+
+    let methods = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b
     };
-    read(expression) {
-        this.expression = expression;
+    this.addMethod = function (name, func) {
+        methods[name] = func;
+    };
+
+    this.calculate = function (expression) {
+        let splitted = expression.split(" ");
+        let firstValue = Number(splitted[0]);
+        let secondValue = Number(splitted[2]);
+        let sign = splitted[1];
+
+        return methods[sign](firstValue, secondValue);
     }
 }
 
--- java-script/4/test/5/4-test.js
+++ java-script/4/test/5/4-test.js
@@ -3,13 +3,38 @@ import chai from "chai";
 
 let calculator;
 before(function () {
-    calculator = new Calculator();
+    calculator = new Calculator;
 });
 
 describe("calculator tests", function () {
     it("sum two correct numbers", function () {
-        calculator.read(3,4);
-        let result = calculator.sum();
-        chai.assert.equal(result, 7);
+        let actualResult = calculator.calculate("3 + 17");
+        chai.assert.equal(actualResult, 20);
     });
+
+    it("subtract two correct numbers", function () {
+        let actualResult = calculator.calculate("20 - 17");
+        chai.assert.equal(actualResult, 3);
+    });
+    it("calculate(12 + 34) = 46", function() {
+        chai.assert.equal(calculator.calculate("12 + 34"), 46);
+      });
+
+      it("calculate(-33 - 22) = -55", function() {
+        chai.assert.equal(calculator.calculate("-33 - 22"), -55);
+      });
+    
+      it("calculate(34 - 12) = 22", function() {
+        chai.assert.equal(calculator.calculate("34 - 12"), 22);
+      });
+    
+      it("add multiplication: calculate(2 * 3) = 6", function() {
+        calculator.addMethod("*", (a, b) => a * b);
+        chai.assert.equal(calculator.calculate("2 * 3"), 6);
+      });
+    
+      it("add power: calculate(2 ** 3) = 8", function() {
+        calculator.addMethod("**", (a, b) => a ** b);
+        chai.assert.equal(calculator.calculate("2 ** 3"), 8);
+      });
 });
