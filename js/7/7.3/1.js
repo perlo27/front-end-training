4e9b59da61142d02ca30c4b9de486559243c4019
Marek_Tabor <marektabor1@gmail.com>
2018-06-20 13:52:54 +0200

    7

---
+++ js/7/7.3/1.js
@@ -0,0 +1,17 @@
+let animal = {
+    jumps: null
+};
+let rabbit = {
+    __proto__: animal,
+    jumps: true
+};
+
+console.log(rabbit.jumps); // true (1)
+
+delete rabbit.jumps;
+
+console.log(rabbit.jumps); // null (2)
+
+delete animal.jumps;
+
+console.log(rabbit.jumps); // undefined (3)
