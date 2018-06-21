cc459161685686caca3808dbc378d959fcb1084c
Marcin <marcin_smola@epam.com>
2018-06-21 09:58:45 +0200

    work in proggress

--- java-script/5/src/11-recursion/1.js
+++ java-script/5/src/11-recursion/1.js
@@ -6,3 +6,20 @@ function sumTo(n) {
 }
 
 console.log(sumTo(100));
+let count = 0;
+function makeCounter() {
+    // let count = 0;
+  
+    return function() {
+      return count++;
+    };
+  }
+  
+  let counter = makeCounter();
+  let counter2 = makeCounter();
+  
+  console.log( counter() ); // 0
+  console.log( counter() ); // 1
+  
+console.log( counter2() ); // ?
+  console.log( counter2() ); // ?
