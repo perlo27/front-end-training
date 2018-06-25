336da649fee01ac0ff8b2df4b320b060854b33ed
Marek_Tabor <marektabor1@gmail.com>
2018-06-25 09:33:59 +0200

    part3 promises

---
+++ js/part3/promises/1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let promise = new Promise(function (resolve, reject) {
+    resolve(1);
+
+    setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(console.log); //1 The second call to resolve is ignored, because only the first call of reject/resolve is taken into account. Further calls are ignored.
