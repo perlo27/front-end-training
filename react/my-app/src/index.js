6e3d34e7acf8f8169ad9ebe12c6ae515b5d02a7f
Marek_Tabor <marektabor1@gmail.com>
2018-06-25 12:34:17 +0200

    task 1 react

---
+++ react/my-app/src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './index.css';
+
+const element = React.createElement(
+    "h1",
+    { className: "title" },
+    "Hello world!"
+);
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div>
+                <Welcome name="Sara"/>
+                <div className="shopping-list">
+                    <h1>Shopping List for {this.props.name}</h1>
+                    <ul>
+                        <li>Instagram</li>
+                        <li>WhatsApp</li>
+                        <li>Oculus</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+render(<ShoppingList name="Mark" />, document.getElementById("root"));
