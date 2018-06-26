9d9ae68b40d31719c33a133704fc484dac7da350
saim-doruklu <saim.doruklu@bilgiedu.net>
2018-06-26 11:39:37 +0200

    tasks

--- week5-react/module1/src/app.js
+++ week5-react/module1/src/app.js
@@ -1,12 +1,42 @@
 import ReactDOM from "react-dom";
+import React from "react";
 import {task1} from "./task1.js";
 import {task3} from "./task3.js";
 
 
 var mountNode = document.getElementById("app");
 
-// const container = task1();
-const container = task3();
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    let index=0;
+    this.tasks = [
+      {name:"Task 1", component:task1()},
+      {name:"Task 3", component:task3()},
+    ].map(task => {
+      Object.assign(task,{id:index++});
+      return task;
+    });
+    this.state = {task:this.tasks[0]};
+  }
+
+  handleTaskSelection(taskId) {
+    this.setState({task:this.tasks[taskId]});
+  }
+
+  render() {
+    return (<div>
+      <ul>
+        {this.tasks.map(task => {
+          const className = this.state.task.id === task.id ? "selectedTask" : "";
+          return <li key={"TaskList-li-"+task.id} className={className} onClick={evt => this.handleTaskSelection(task.id)}>{task.name}</li>
+        })}
+      </ul>
+      <span>Task : {this.state.task.name}</span>
+      {this.state.task.component}
+    </div>);
+  }
+}
 
 
-ReactDOM.render(container, mountNode);
+ReactDOM.render(<TaskList />, mountNode);
