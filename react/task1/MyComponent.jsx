a3935346ec134fbe14285e9a2732ee9eec002fc5
Marcin <marcin_smola@epam.com>
2018-06-26 14:07:52 +0200

    react hello world, added webpack

---
+++ react/task1/MyComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+var MyComponent = React.createClass({
+    render() {
+       return <div>
+                 <h1>Hello World!</h1>
+                 <p>This is my first React Component.</p>
+              </div>
+       }
+ 
+ });
+
+ export default MyComponent;
---
+++ react/task1/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.jsx';
+// import MyComponent from './MyComponent.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<MyComponent />, document.getElementById('react-component'));
