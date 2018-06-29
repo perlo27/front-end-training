3874c1eeae08e56f2a42112565494e9c85e0ea15
Mateusz Rataj <Mateusz_Rataj@epam.com>
2018-06-29 10:31:03 +0200

    Search bar text capturing

--- react-training/src/header/MovieSearch.jsx
+++ react-training/src/header/MovieSearch.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 export default class MovieSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   state = {
-    searchText: ""
+    value: "",
   };
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
   render() {
-    return <input className="search-bar" type="text" />;
+    return <input className="search-bar" value={this.state.value} type="text" onChange={this.handleChange} />;
   }
 }
