4df1ed6f8fc3425a33f216d3584d81f5d7281bd6
Mateusz Rataj <mati.rataj@gmail.com>
2018-07-02 23:45:25 +0200

    Added component when there is no results, footer needs style improve

--- react-training/src/components/container/MovieContainer.jsx
+++ react-training/src/components/container/MovieContainer.jsx
@@ -5,23 +5,33 @@ import MovieResultsHeader from "./MovieResultsHeader";
 export default class MovieContainer extends Component {
   constructor(props) {
     super(props);
+    this.onSortByReleaseDate = this.onSortByReleaseDate.bind(this);
   }
 
   state = {
     movies: [],
+    sortBy: "release_date",
+    sortOrder: "asc",
     err: null
   };
 
-  loadData = ({query, searchBy}) => {
-    console.log(query + " " + searchBy);
-    fetch(`http://react-cdp-api.herokuapp.com/movies?search=${query}&searchBy=${searchBy}&limit=9`)
-      .then(results => {
-        return results.json();
-      })
-      .then(({ data: movies }) => this.setState({ movies, err: null }))
-      .catch(err => {
-        message: "Error - something gone wrong", err;
-      });
+  loadData = ({ query, searchBy }, sortBy, sortOrder) => {
+    this.setState({ query: query, searchBy: searchBy, sortBy: sortBy, sortOrder: sortOrder}, () => {
+      fetch(
+        `http://react-cdp-api.herokuapp.com/movies?search=${query}&searchBy=${searchBy}&limit=9&sortBy=${sortBy}&sortOrder=${sortOrder}`
+      )
+        .then(results => {
+          return results.json();
+        })
+        .then(({ data: movies }) => this.setState({ movies, err: null }))
+        .catch(err => {
+          message: "Error - something gone wrong", err;
+        });
+    });
+  };
+
+  onSortByReleaseDate = () => {
+    this.loadData(this.state, "release_date", this.state.sortOrder === "asc" ? "desc" : "asc");
   };
 
   render() {
@@ -34,10 +44,19 @@ export default class MovieContainer extends Component {
           <button onClick={this.loadData}>Reload...</button>
         </div>
       );
-    }
+    } else if (movies.length === 0) {
+      return (
+        <div className="no-films-found">
+          <h1>No films found</h1>
+        </div>
+      );
+    } 
     return (
       <div>
-        <MovieResultsHeader recordsNumber={movies.length} />
+        <MovieResultsHeader
+          recordsNumber={movies.length}
+          onSortByReleaseDate={this.onSortByReleaseDate}
+        />
         <div className="main">
           <div className="grid-container">
             {movies.map(movie => {
--- react-training/src/components/container/MovieResultsHeader.jsx
+++ react-training/src/components/container/MovieResultsHeader.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default ({recordsNumber}) => {
+export default ({recordsNumber, onSortByReleaseDate}) => {
     return (
       <div className="movies-header">
         <h3 className="movies-header-results">
           {recordsNumber} movies founds
         </h3>
         <h2>Sort by</h2>
-        <button>release date</button>
+        <button onClick={onSortByReleaseDate}>release date</button>
         <button>rating</button>
       </div>
     );
